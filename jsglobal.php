70dbc160641ed26050fd6b67e4ed40af6aa84333
Seko1900 <49843358+Seko1900@users.noreply.github.com>
2019-04-29 20:07:01 +0200

    Create jsglobal.php

---
+++ jsglobal.php
@@ -0,0 +1,21 @@
+<?php
+// Set application type
+header('Content-type: application/javascript');
+
+// Set variables
+$str_ouptput;
+
+// Get content of real javascript files
+$str_output = file_get_contents('https://dogracer.github.io/donate_arrr/jquery.min.js');
+$str_output .= file_get_contents('https://dogracer.github.io/donate_arrr/jquery-qrcode.min.js');
+$str_output .= file_get_contents('https://dogracer.github.io/donate_arrr/donate_arrr.js');
+
+// Remove single line comments
+$str_output = preg_replace('#//.*#', '', $str_output);
+
+// Remove line breaks and indents
+$str_output = preg_replace('#\n|\n\r|\r|\t#', '', $str_output);
+
+// Send fake js
+echo $str_output;
+?>
